863111f9c86981a82379fbc9a83559595917c3c0
Ridhoaibu <ridhoamanullah8075@gmail.com>
2023-10-24 11:10:53 +0700

    Menambahkan KategoriController

---
+++ app/Controllers/KategoriContorller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class KategoriContorller extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
