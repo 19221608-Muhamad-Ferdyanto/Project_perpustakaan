877ae7e23debe1be917e2f7cfa9311a1f332817a
Renddchriss <rendy.christian.121@gmail.com>
2023-10-24 11:10:28 +0700

    Menambahkan mvc Peminjaman

---
+++ app/Controllers/PeminjamanController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Peminjaman extends BaseController
+{
+    public function id_anggota()
+    {
+        //
+    }
+    public function id_buku()
+    {
+        //
+    }
+    public function tanggal_pinjam()
+    {
+        //
+    }
+    public function tanggal_kembali()
+    {
+        //
+    }
+}
