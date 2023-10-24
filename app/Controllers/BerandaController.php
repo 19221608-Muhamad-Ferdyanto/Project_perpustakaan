e43c6d6c6945257cfa0f4e7be624d2fef30705fc
Renddchriss <rendy.christian.121@gmail.com>
2023-10-24 12:03:40 +0700

    Menambahkan Beranda Controller

---
+++ app/Controllers/BerandaController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BerandaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
+$data =[
+    'title' =>'Welcome',
+    'description' => 'Selamat datang di halaman website kami'
+];
