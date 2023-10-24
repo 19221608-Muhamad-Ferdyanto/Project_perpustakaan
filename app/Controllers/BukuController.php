96dd350ce221beab1dfb6f88f2d737c8cc7b82c3
Dwi-Kurniawati-19221078 <dwikurniawatihrd@gmail.com>
2023-10-24 11:44:30 +0700

    Menambahkan buku controller

---
+++ app/Controllers/BukuController.php
@@ -0,0 +1,38 @@
+
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class BukuController extends BaseController
+{
+    public function judul()
+    {
+        //
+    }
+    public function pengarang()
+    {
+        //
+    }
+    public function tahun_terbit()
+    {
+        //
+    }
+    public function id_kategori()
+    {
+        //
+    }
+    public function id_penerbit()
+    {
+        //
+    }
+    public function jumlah_halaman ()
+    {
+        //
+    }
+    public function stok()
+    {
+        //
+    }
+}
